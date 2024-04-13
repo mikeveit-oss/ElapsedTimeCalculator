804f29ea83842288bd3a18858623e89427507255
mike-opnsrc <mike@opnsrcsolutions.com>
2024-04-13 11:42:08 -0400

    set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
+var startTime, endTime, elapsed;
+
+function calculate() {
+  var timeDiff = endTime - startTime; //in ms 
+  // strip the ms 
+  timeDiff /= 1000; 
+  
+  // get seconds 
+  var seconds = Math.round(timeDiff);
+  elapsed = seconds;
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br />
+      <label>
+          Start Time: <input name="startTime" value={startTime} />
+     </label>
+      <br />
+      <br />
+      <label>
+          End Time: <input name="endTime" value={endTime} />
+     </label>
+     <br />
+     <br />
+     <button onClick={calculate}>Calculate</button>
+     <br />
+     <br />
+     Elapsed Time = {elapsed} seconds
     </div>
   );
 }
